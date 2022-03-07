fe8f1b88fcfa5c8e9ae4061b17e1ddbe3e97cbc6
Parichay Gupta <44670041+ParichayGupta@users.noreply.github.com>
2022-03-07 22:28:41 +0530

    uploaded complete porject

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegistrationService } from '../register/registration.service';
+import { User } from '../model/user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user = new User();
+  errorMessage = '';
+  constructor(private _service: RegistrationService, private _route: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  userLogin() {
+    this._service.loginUserFromRemote(this.user).subscribe(
+      data => {
+        console.log("Response Received"),
+        this._route.navigate(['/home'])
+      },
+      error => {
+        console.log("Exception Occurred"),
+        this.errorMessage = "Bad Credentials: Email or Password is invalid !!"
+      }
+    )
+  }
+
+}
